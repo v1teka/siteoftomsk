8ef75c07ccd9fe5fa8a78d8c85c19cdabdbc8772
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-17 22:28:19 +0700

    Фикс ошибки при миграции

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
@@ -16,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Фикс ошибки Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+        // https://laravel-news.com/laravel-5-4-key-too-long-error
+        Schema::defaultStringLength(191);
+
         // Валидатор пароля пользователя
         Validator::extend('user_password', function ($attribute, $value, $parameters, $validator) {
             return Hash::check($value, Auth::user()->password);
