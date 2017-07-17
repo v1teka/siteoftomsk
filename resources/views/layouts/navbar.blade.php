3c0bb899e1d28e461cd1e39719b935a6dc1d28ef
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-17 13:43:46 +0700

    Добавлено верхнее меню

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,1 +1,17 @@
-Navbar
+<div class="navbar">
+    <div class="navbar__logo">
+        <a class="link navbar__logo-link" href="/" title="Томск 7.0. Технологии и творчество">Томск 7.0</a>
+    </div>
+    <div class="navbar__content">
+        <a class="link navbar__link" href="">О проекте</a>
+        <a class="link navbar__link" href="">Новости</a>
+        <a class="link navbar__link" href="">Карта позитива</a>
+        <a class="link navbar__link" href="">Экология</a>
+        <a class="link navbar__link" href="">Креатив</a>
+        <a class="link navbar__link" href="">Мнения</a>
+    </div>
+    <div class="navbar__right">
+        <a class="link navbar__link" href="">Вход</a>
+        <a class="link navbar__link" href="">Регистрация</a>
+    </div>
+</div>
