84a5761d1e70088de3ea6cd26ff1fdde1d3b7142
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-17 13:28:08 +0700

    Добавлен footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,7 @@
-Footer
+<div class="footer">
+    <div class="container footer__container">
+        <div class="footer__copyright">&copy; Томск 7.0. Технологии и творчество, {{ date('Y') }}</div>
+        <div class="footer__copyright">При полном или частичном использовании материалов сайта ссылка обязательна.</div>
+        <div class="footer__author">Сделал <a class="link" href="http://solopchenko.net" target="_blank">Святослав Солопченко</a> в 2017 году.</div>
+    </div>
+</div>
