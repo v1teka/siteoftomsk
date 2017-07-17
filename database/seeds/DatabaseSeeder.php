db80c1cbcfc7ebafd08f69554f0cc90197db3e22
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-17 20:20:56 +0700

    Скрипт добавления модератора

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Role;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+    }
+}
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->delete();
+        Role::create(['name' => 'Модератор']);
+    }
+}
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->delete();
+        $moderator = User::create(['name' => 'Святолсав', 'surname' => 'Солопченко', 'email' => 'hi@solopchenko.net', 'password' => bcrypt('password')]);
+        $moderator_role = Role::where('name', 'Модератор')->first();
+        $moderator->roles()->attach($moderator_role->id);
     }
 }
