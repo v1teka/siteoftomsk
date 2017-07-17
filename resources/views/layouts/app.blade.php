e3f8615e6f46b0f7763d7c1ec0c1617a4d53eb37
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-17 12:59:55 +0700

    Подготовка шаблонов

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+  <head>
+    {{-- Basic Page Needs
+    –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="description" content="@yield('description')">
+    <meta name="author" content="">
+
+    {{-- Mobile Specific Metas
+    –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- Шрифты
+    –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+    <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700&amp;subset=cyrillic" rel="stylesheet">
+
+    {{-- CSS
+    –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+    <link rel="stylesheet" href="{{ asset('/assets/app.css') }}">
+    @stack('head')
+  </head>
+
+  <body>
+    {{-- Верхнее меню
+    –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+    @include('layouts.navbar')
+
+    {{-- Контент из дочерних шаблонов
+    –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+
+    @yield('content')
+    {{-- Подвал
+    –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+    @include('layouts.footer')
+  </body>
+
+  {{-- Скрипты
+  –––––––––––––––––––––––––––––––––––––––––––––––––– --}}
+  <script src="{{ asset('/assets/app.js') }}" type="text/javascript"></script>
+  @stack('scripts')
+</html>
