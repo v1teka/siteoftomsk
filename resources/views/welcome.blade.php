cbdcbd3c95083db66a30b7cc33d9505b61bfda40
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-17 13:58:54 +0700

    Добавлены секции

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,10 @@
 
 @section('content')
   @parent
-  Content
+  <div class="section welcome">
+      <div class="container welcome__container">
+          <div class="title title-xl welcome__title">Уважаемые посетители портала,</div>
+          <div class="text welcome__text">здесь Вы можете ознакомиться с проектами по социологическим исследованиям в городе Томске. На страницах нашего портала Вы можете принять участие в разных опросах, оставить свои комментарии и просто узнать очень много интересного!</div>
+      </div>
+  </div>
 @endsection
