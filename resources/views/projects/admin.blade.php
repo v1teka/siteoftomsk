57baa5615cb5ab23f609c355651d007eb42a6491
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-22 12:53:02 +0700

    Изменены поля в таблице с проектами в админке

--- resources/views/projects/admin.blade.php
+++ resources/views/projects/admin.blade.php
@@ -12,9 +12,9 @@
                     <tr class="table__row">
                         <td class="table__column">№</td>
                         <td class="table__column">Название</td>
-                        <td class="table__column">Описание</td>
                         <td class="table__column">Рубрика</td>
                         <td class="table__column">Автор</td>
+                        <td class="table__column">Дата создания</td>
                         <td class="table__column">Модерация</td>
                         <td class="table__column">Опубликован</td>
                     </tr>
@@ -24,13 +24,15 @@
                         <tr class="table__row">
                             <td class="table__column">{{ $project->id }}</td>
                             <td class="table__column"><a class="link" href="{{ route('projects.show', $project) }}">{{ $project->title }}</td>
-                            <td class="table__column">{{ $project->description }}</td>
                             <td class="table__column">
                                 @if($project->rubric)
                                     <a class="link" href="{{ route('rubrics.show', $project->rubric) }}">{{ $project->rubric->name }}</a>
+                                @else
+                                    Без рубрики
                                 @endif
                             </td>
-                            <td class="table__column">{{ $project->user->full_name }}</td>
+                            <td class="table__column"><a class="link" href="{{ route('users.show', $project->user) }}">{{ $project->user->full_name }}</a></td>
+                            <td class="table__column">{{ $project->created_at->format('d.m.Y H:i:s') }}</td>
                             <td class="table__column">
                                 @if ($project->moderated === null)
                                     Рассматривается
