aac0b1e6b14b60a5bed17c06d20797c3ac7a70b3
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-26 22:49:18 +0700

    Изменен код js спойлера

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,17 +1,7 @@
 $(document).ready(function() {
     $('.js-spoiler-link').click(function(e) {
-        var link = $(this);
         var content = $(this).next('.js-spoiler-content').first();
-        if (content.hasClass('js-spoiler-content-showed')) {
-            content.slideUp(500);
-            content.removeClass('js-spoiler-content-showed');
-            link.html('Показать');
-        }
-        else {
-            content.slideDown(500);
-            content.addClass('js-spoiler-content-showed');
-            link.html('Скрыть');
-        }
+        content.slideToggle(500);
         return false;
     });
 });
