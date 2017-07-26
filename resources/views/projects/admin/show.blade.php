24550de65e70fcd2d00bb84ea2d2442d3d715dd5
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-26 16:11:13 +0700

    Содержимое проекта скрывается под спойлером в админке

--- resources/views/projects/admin/show.blade.php
+++ resources/views/projects/admin/show.blade.php
@@ -18,7 +18,12 @@
                 </div>
                 <div class="form-group">
                     <label class="form-group__label">Контент</label>
-                    <div class="form-group__value">{{ $project->content }}</div>
+                    <div class="form-group__value">
+                        <a class="link link--dashed js-spoiler-link" href="#">Показать</a>
+                        <div class="project-content js-spoiler-content">
+                            {!! $project->content !!}
+                        </div>
+                    </div>
                 </div>
                 <div class="form-group">
                     <label class="form-group__label">Изображение</label>
