af92cfab57ed1773281ff0d5b45776be2f710462
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-07-28 16:01:34 +0700

    Добавлены партнёры

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,24 @@
 <div class="footer">
     <div class="container">
-        <div class="footer__copyright">&copy; Томск 7.0 Технологии и творчество, {{ date('Y') }}</div>
-        <div class="footer__copyright">При полном или частичном использовании материалов сайта ссылка обязательна.</div>
-        <div class="footer__author">Сделал <a class="link link--gray" href="http://solopchenko.net" target="_blank">Святослав Солопченко</a> в 2017 году.</div>
+        <div class="footer-partners">
+            <div class="footer-partners__title">Партнёры</div>
+            <div class="row">
+                <div class="row__col row__col--size-3 footer-partners__partner">
+                    <a href="http://tomsk.gov.ru" target="_blank"><img class="footer-partners__img" src="/assets/images/tomsk_logo.png" height="44"/></a>
+                </div>
+                <div class="row__col row__col--size-3 footer-partners__partner">
+                    <a href="http://tpu.ru" target="_blank"><img class="footer-partners__img" src="/assets/images/tpu_logo.png" height="44"/></a>
+                </div>
+                <div class="row__col row__col--size-3 footer-partners__partner">
+                </div>
+                <div class="row__col row__col--size-3 footer-partners__partner">
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div>&copy; <a class="link link--dark" href="{{ route('pages.index') }}">Томск 7.0 Технологии и творчество</a>, {{ date('Y') }}</div>
+            <div>При полном или частичном использовании материалов сайта ссылка обязательна.</div>
+            <div class="footer-copyright__author">Сделал <a class="link link--dark" href="http://solopchenko.net" target="_blank">Святослав Солопченко</a> в 2017 году.</div>
+        </div>
     </div>
 </div>
