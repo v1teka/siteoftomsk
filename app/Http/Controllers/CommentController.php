a580711d8c0266351e19cf412bf91ce4011fef9d
SMike <smike@tpu.ru>
2017-09-26 11:14:43 +0700

    Добавление reCAPTCHA при отправке комментария

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -6,21 +6,31 @@ use Illuminate\Http\Request;
 use App\Project;
 use App\Comment;
 use Auth;
+use Unikent\Curl\Facades\Curl;
 
 class CommentController extends Controller
 {
     public function store(Project $project) {
         $this->validate(request(), [
             'message' => 'required',
+            'g-recaptcha-response' => 'required',
         ]);
 
-        $comment = new Comment;
-        $comment->project_id = $project->id;
-        $comment->message = request('message');
-        $comment->created_by = Auth::user()->id;
-        $comment->updated_by = Auth::user()->id;
-        $comment->save();
+        $verification = json_decode(Curl::simple_get('https://www.google.com/recaptcha/api/siteverify',
+            ['secret' => '6Lft_TEUAAAAAKNtlOKVa6WaVDDo0Np5eretvL0X', 'response' => request('g-recaptcha-response')]));
+        if ($verification->success) {
+            $comment = new Comment;
+            $comment->project_id = $project->id;
+            $comment->message = request('message');
+            $comment->created_by = Auth::user()->id;
+            $comment->updated_by = Auth::user()->id;
+            $comment->save();
 
-        return redirect()->route('projects.show', $project);
+            return redirect()->route('projects.show', $project);
+        } else {
+            return redirect()->back()->withErrors(['error' => 'Проверка reCAPTCHA не пройдена.']);
+        }
+
+        
     }
 }
--- resources/views/projects/includes/comment_form.blade.php
+++ resources/views/projects/includes/comment_form.blade.php
@@ -1,7 +1,16 @@
+@push('scripts')
+<script src='https://www.google.com/recaptcha/api.js'></script>
+@endpush
+
 @if ($canComment == 1)
     <div class="container-main" xmlns="http://www.w3.org/1999/html">
         <form method="POST" action="{{ route('projects.addcomment', $project) }}">
             {{ csrf_field() }}
+            @if ($errors->has('error'))
+                <div class="alert alert-danger">
+                    {!! $errors->get('error')[0] !!}
+                </div>
+            @endif
             <div class="form-group">
                 <label for="message">Добавить комментарий</label>
                 <textarea class="form-control {{ $errors->has('message') ? 'input_has-error' : '' }}" type="text" name="message" id="message" placeholder="Комментарий">{{ $message or old('message') }}</textarea>
@@ -9,6 +18,7 @@
                     <div class="form-group__message form-group__message--error">{{ $errors->first('message') }}</div>
                 @endif
             </div>
+            <div class="g-recaptcha" data-sitekey="6Lft_TEUAAAAAJCmLC9Io66zr8wq9Clwxm87F_J7"></div>
             <div class="form-group">
                 <button class="btn btn-success" type="submit" data-toggle="tooltop" title="Комментарий будет рассмотрен модератором">Отправить на проверку</button>
             </div>
