fa8e37848d50535780886abf21bfe2c8cf6e9240
SMike <smike@tpu.ru>
2017-10-25 11:39:22 +0700

    Удалил надпись "Уважаемые посетители" под слайдером

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -9,14 +9,14 @@
       @include ('pages.includes.slider')
   </div>
 
-  <div class="section welcome">
+  <!--<div class="section welcome">
       <div class="container-fluid">
           <div class="alert alert-success" role="alert">
               <div class="title h2">Уважаемые посетители!</div>
               <p>На нашем портале вы можете получить информацию и выразить свое мнение по вопросам, связанным с формированием комфортной и безопасной городской среды, отслеживать динамику процесса  формирования Томска как города равных возможностей.</p>
           </div>
       </div>
-  </div>
+  </div>-->
 
   @if (count($projects))
       <div class="section section--last projects">
