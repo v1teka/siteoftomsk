98c7d01e05a7d27a28e47c8b3e118a5b95b29583
SMike <smike@tpu.ru>
2017-11-02 10:32:49 +0700

    Добавление изображений в слайдер. Автоматическое изменение картинок в слайдере

--- resources/views/pages/includes/slider.blade.php
+++ resources/views/pages/includes/slider.blade.php
@@ -1,36 +1,14 @@
-<div class="fotorama">
-    <div class="slider-item" data-img="https://pp.userapi.com/c627231/v627231559/2c8f8/fVt8BKPEZjY.jpg">
-        <div class="container">
-            <div class="title title--xxl slider-item__title">ДОБРО ПОЖАЛОВАТЬ В ТОМСК 7.0</div>
-            <div class="slider-item__content">
-                <!--<p>Технологии и творчество</p>-->
-                <p class="slider_info">Уважаемые посетители!</p>
-                <p class="slider_small">На нашем портале вы можете получить информацию и выразить свое мнение по вопросам,</p>
-                <p class="slider_small">связанным с формированием комфортной и безопасной городской среды</p>
-                <div class="slider_questioning_container">
-                    <div class="slider_questioning_left_div">
-                        <a class="slider_questioning_left_a" target="_blank" href="https://goo.gl/forms/YwgnGCsbY2nunFAq2" class="questioning-link">Пройти анкетирование</a>
-                    </div>
-                    <div class="slider_questioning_right_div">
-                        <a class="slider_questioning_right_a" href="{{ route('forum.index') }}">Город равных возможностей</a>
-                    </div>
-                    <div class="clear-both"></div>
-                </div>
-            </div>
+<div class="fotorama" data-autoplay="true">
+    <?php $i = 1;?>
+    @while (file_exists(public_path() . '\\assets\\slider\\' . $i . '.jpg'))
+        <div class="slider-item" data-img="/assets/slider/{!! $i !!}.jpg">
+            @include ('pages.includes.slider_text')
         </div>
-    </div>
-    <div class="slider-item slider-item--center" data-img="https://pp.userapi.com/c627231/v627231559/2c901/kpbqrMikGYo.jpg">
-        <div class="container">
-            <div class="title title--xxl slider-item__title">Томск 7.0</div>
-            <div class="slider-item__content">
-                <!--<p>Технологии и творчество</p>-->
-                <!--<a target="_blank" href="https://goo.gl/forms/YwgnGCsbY2nunFAq2" class="questioning-link">Пройти анкетирование</a>-->
-            </div>
-        </div>
-    </div>
-    <a href="https://youtu.be/Q8CUYhxJFrw?rel=0&amp;controls=0&amp;showinfo=0&amp;modestbranding=1">
+        <?php $i++; ?>
+    @endwhile
+    <!--<a href="https://youtu.be/Q8CUYhxJFrw?rel=0&amp;controls=0&amp;showinfo=0&amp;modestbranding=1">
         <img src="https://i.ytimg.com/vi/5f9TEzIaeLQ/maxresdefault.jpg" />
-    </a>
+    </a>-->
 </div>
 @push('head')
     <!-- fotorama.css -->
---
+++ resources/views/pages/includes/slider_text.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="title title--xxl slider-item__title">ДОБРО ПОЖАЛОВАТЬ В ТОМСК 7.0</div>
+    <div class="slider-item__content">
+        <p class="slider_info">Уважаемые посетители!</p>
+        <p class="slider_small">На нашем портале вы можете получить информацию и выразить свое мнение по вопросам,</p>
+        <p class="slider_small">связанным с формированием комфортной и безопасной городской среды</p>
+        <div class="slider_questioning_container">
+            <div class="slider_questioning_left_div">
+                <a class="slider_questioning_left_a" target="_blank" href="https://goo.gl/forms/YwgnGCsbY2nunFAq2" class="questioning-link">Пройти анкетирование</a>
+            </div>
+            <div class="slider_questioning_right_div">
+                <a class="slider_questioning_right_a" href="{{ route('forum.index') }}">Город равных возможностей</a>
+            </div>
+            <div class="clear-both"></div>
+        </div>
+    </div>
+</div>
